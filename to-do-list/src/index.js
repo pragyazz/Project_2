aa60d2693e603364acb8cc86865414089154be77
pragyazz <pragyagun@gmail.com>
2022-08-17 15:07:08 +0530

    second

--- to-do-list/src/index.js
+++ to-do-list/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <App></App>
+  </>,
   document.getElementById('root')
 );
 
